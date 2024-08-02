8758b0b14bb702068a80166e497b6cb3c74c4355
Yashavant Singh <yashavantsingh01.hp@outlook.com>
2024-08-02 17:19:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const person = {
+    firstname: "Yash",
+    lastname: "Singh",
+    dob: "07-07-2001"
+};
+
+document.getElementById("firstname").textContent = person.firstname;
+document.getElementById("lastname").textContent = person.lastname;
+document.getElementById("dob").textContent = person.dob;
+
+
